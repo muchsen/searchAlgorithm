25a35ca005edd073aea2696ae64fe5818025255b
TA1059 <muchsen.ft21@mail.umy.ac.id>
2022-10-24 16:10:19 +0700

    menambahkan int array

--- searchAlgorithm/Program.cs
+++ searchAlgorithm/Program.cs
@@ -1,1 +1,12 @@
 ﻿using System;
+
+namespace SearchAlgorthm
+{
+    class Program
+    {
+        //Array to be searched
+        int[] arr = new int[20];
+        //Get the number of element to store in the array
+        int i;
+    }
+}
