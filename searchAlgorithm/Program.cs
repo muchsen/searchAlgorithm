34b60256061ef596e84f956f78e26370857801b4
TA1059 <muchsen.ft21@mail.umy.ac.id>
2022-10-24 16:27:20 +0700

    menerima element array

--- searchAlgorithm/Program.cs
+++ searchAlgorithm/Program.cs
@@ -6,7 +6,37 @@ namespace SearchAlgorthm
     {
         //Array to be searched
         int[] arr = new int[20];
+        //Number of elements in the array
+        int n;
         //Get the number of element to store in the array
         int i;
+
+        public void input ()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number of elemets in the array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 20))
+                    break;
+                else
+                    Console.WriteLine("\nArray should have minimum 1 and maximum 20 elements.\n");
+                
+            }
+            //Accept array elements
+            Console.WriteLine("");
+            Console.WriteLine("---------------------");
+            Console.WriteLine(" Enter array elements");
+            Console.WriteLine("---------------------");
+            for (i = 0; i < n; i++) ;
+            {
+                Console.WriteLine("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
+            
+
+        }
     }
 }
