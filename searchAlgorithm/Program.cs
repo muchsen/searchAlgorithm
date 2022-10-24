9d13cd7b66ce19d9b0d8ac0049602372c235b344
TA1059 <muchsen.ft21@mail.umy.ac.id>
2022-10-24 17:25:51 +0700

    menambahkan switch case

--- searchAlgorithm/Program.cs
+++ searchAlgorithm/Program.cs
@@ -110,6 +110,52 @@ namespace SearchAlgorthm
             
 
         }
+        static void Main(string[] args)
+        {
+            Program mylist = new Program();
+            int pilihanmenu;
+            do
+            {
+                Console.WriteLine("Menu option");
+                Console.WriteLine("================");
+                Console.WriteLine("1.Linear Search");
+                Console.WriteLine("2.Binary Search");
+                Console.WriteLine("3.Exit");
+                Console.WriteLine("Enter your choice (1,2,3 : ");
+                pilihanmenu = Convert.ToInt32(Console.ReadLine());
+
+                switch (pilihanmenu)
+                {
+                    case 1:
+                        Console.WriteLine("");
+                        Console.WriteLine("---------------------");
+                        Console.WriteLine("Linear Search ");
+                        Console.WriteLine("---------------------");
+                        mylist.input();
+                        mylist.LinearSearch();
+                        break;
+                    case 2:
+                        Console.WriteLine("");
+                        Console.WriteLine("---------------------");
+                        Console.WriteLine("Binary Search ");
+                        Console.WriteLine("---------------------");
+                        mylist.input();
+                        mylist.BinarySearch();
+                        break ;
+                    case 3:
+                        Console.WriteLine("exit.");
+                        break ;
+                    default:
+                        Console.WriteLine("error");
+                        break ;
+
+
+                }
+                //to exit from the console 
+                Console.WriteLine("\n\nPress Return to exit");
+                Console.ReadLine();
+            }while (pilihanmenu != 3);
+        }
 
     }
 }
